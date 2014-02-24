f0ecbc2c0a31a75d3b945b5d14f85ce360cf4b6e
lev-kazakov <lev.kazakov@gmail.com>
2014-02-24 23:48:49 +0200

    final push

--- TipCalculator/src/il/ac/huji/tipcalculator/TipCalculatorActivity.java
+++ TipCalculator/src/il/ac/huji/tipcalculator/TipCalculatorActivity.java
@@ -1,11 +1,19 @@
 package il.ac.huji.tipcalculator;
 
-import android.os.Bundle;
+import java.text.DecimalFormat;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TipCalculatorActivity extends Activity {
 
+	public static final double TIP_PERCENT = 0.12;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +26,31 @@ public class TipCalculatorActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    /** Called when the user clicks the Calculate button */
+    public void calcTip(View view) {
+         EditText billAmount = (EditText) findViewById(R.id.edtBillAmount);
+         TextView tipResult = (TextView) findViewById(R.id.txtTipResult);
+         
+         double bill;
+         try {
+        	 bill = Double.parseDouble(billAmount.getText().toString());
+         } catch (NumberFormatException e) {
+        	 bill = 0;
+         }
+         
+         double tip = bill*TIP_PERCENT;
+         
+         String format = "#.##";
+         
+         if( ((CheckBox) findViewById(R.id.chkRound)).isChecked() ) {
+        	 	format = "#";
+         }
+         
+         DecimalFormat df = new DecimalFormat(format);
+         
+         tipResult.setText("Tip: $" + df.format(tip));
     }
     
 }
